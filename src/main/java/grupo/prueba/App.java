81db4dfb4d61ccc2a9ff9a172489067b1da52d93
Ever Rangel <everardo.flores@elektra.com.mx>
2019-12-02 17:57:10 -0600

    Nuevo TOKEN sonar

--- src/main/java/grupo/prueba/App.java
+++ src/main/java/grupo/prueba/App.java
@@ -8,17 +8,13 @@ import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
 
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     private static final Logger LOGGER = LogManager.getLogger(App.class);
 
     public static void main( String[] args )
     {
-        //setLogLevel(Level.INFO);
+        setLogLevel(Level.INFO);
         LOGGER.trace("Log level trace");
         LOGGER.debug("Log level debug");
         LOGGER.info("Log level info");
@@ -32,13 +28,12 @@ public class App
      *
      * @param l The log level to set
      */
-    /*
-    public static void setLogLevel(Level l) {
+    private static void setLogLevel(Level l) {
         LOGGER.info("Setting log level to " + l.name());
         LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
         Configuration conf = ctx.getConfiguration();
         conf.getLoggerConfig(LogManager.ROOT_LOGGER_NAME).setLevel(l);
         ctx.updateLoggers(conf);
     }
-    */
+
 }
